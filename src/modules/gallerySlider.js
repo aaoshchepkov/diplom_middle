ef9ea6bb4bddd668a0dd0734f8f280d43d3b138f
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-03-23 23:22:08 +0700

    слайдер фотогаллерея

--- src/modules/gallerySlider.js
+++ src/modules/gallerySlider.js
@@ -8,6 +8,34 @@ const gallerySlider = () => {
       let li;
       let ul;
       let button;
+      let arrowLeft;
+      let arrowRight;
+      let spanR;
+      let spanL;
+      arrowLeft = document.createElement("span");
+      arrowLeft.classList.add("slider-arrow");
+      arrowLeft.classList.add("slider-arrow-prev");
+      dotsWrapper.appendChild(arrowLeft);
+      spanL = document.createElement("span");
+      arrowLeft.style.cssText = `left : 20px;`;
+      spanL.style.cssText = `background-image: url(./images/arrow-left.png); background-repeat: no-repeat;
+      background-position: center;
+      z-index: 10000;`;
+      arrowLeft.appendChild(spanL);
+
+
+      arrowRight = document.createElement("span");
+      arrowRight.classList.add("slider-arrow");
+      arrowRight.classList.add("slider-arrow-next");
+      dotsWrapper.prepend(arrowRight);
+      spanR = document.createElement("span");
+      arrowRight.style.cssText = `right : 20px;`;
+      spanR.style.cssText = `background-image: url(./images/arrow-right.png); background-repeat: no-repeat;
+      background-position: center;
+      z-index: 10000;`;
+      arrowRight.appendChild(spanR);
+
+      
       ul = document.createElement("ul");
       ul.classList.add("slider-dots");
       dotsWrapper.appendChild(ul);
@@ -31,6 +59,8 @@ const gallerySlider = () => {
     const slider = document.querySelector(".gallery-slider");
     const dot = document.querySelectorAll(".slider-dots > li");
     const button = document.querySelectorAll(".dot");
+    const buttonL = document.querySelector(".slider-arrow-next > span");
+    const buttonR = document.querySelector(".slider-arrow-prev > span");
     let interval;
     let currentSlide = 0;
     slide.forEach((item, i) => {
@@ -59,13 +89,19 @@ const gallerySlider = () => {
 
     slider.addEventListener("click", (event) => {
       event.preventDefault();
+      
       let target = event.target;
-      if (!target.matches(".dot")) {
+      
+      if (!target.matches(".dot") && target !== buttonL && target !== buttonR) {
         return;
       }
       slide[currentSlide].style.display = "none";
       dot[currentSlide].classList.remove("slick-active");
-      if (target.matches(".dot")) {
+      if (target === buttonL) {
+        currentSlide++;
+      } else if (target === buttonR) {
+        currentSlide--;
+      } else if (target.matches(".dot")) {
         button.forEach((elem, index) => {
           if (target === elem) {
             currentSlide = index;
@@ -84,13 +120,13 @@ const gallerySlider = () => {
 
     slider.addEventListener("mouseover", (event) => {
       let target = event.target;
-      if (target.closest(".slide") || target.closest(".slider-dot")) {
+      if (target.closest(".slide") || target.closest(".slider-arrow") || target.closest(".slider-arrow")) {
         stopSlide();
       }
     });
     slider.addEventListener("mouseout", (event) => {
       let target = event.target;
-      if (target.closest(".slide") || target.closest(".slider-dot")) {
+      if (target.closest(".slide") || target.closest(".slider-arrow") || target.closest(".slider-arrow"))  {
         startSlide();
       }
     });
